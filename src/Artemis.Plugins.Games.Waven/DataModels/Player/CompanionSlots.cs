5a7444f7ae6cc202fc756ccd9e15f337004e2eba
Nassime Mountasir <nassimem@live.fr>
2023-09-16 15:02:30 +0200

    added companion and spells info

---
+++ src/Artemis.Plugins.Games.Waven/DataModels/Player/CompanionSlots.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artemis.Plugins.Games.Waven.DataModels.Player
+{
+    public class CompanionSlots
+    {
+        public Companion? Slot1 { get; set; }
+        public Companion? Slot2 { get; set; }
+        public Companion? Slot3 { get; set; }
+        public Companion? Slot4 { get; set; }
+        public CompanionSlots() { }
+    }
+}
